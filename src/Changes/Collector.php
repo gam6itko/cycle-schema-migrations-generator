5c8eed39c84b75b721f49ee7c89ab924a3a7a95d
Alexander Strizhak <gam6itko@gmail.com>
2024-11-22 15:28:14 +0300

    try to fix mssql tests

--- src/Changes/Collector.php
+++ src/Changes/Collector.php
@@ -15,58 +15,58 @@ final class Collector
 
         foreach ($atomizer->getTables() as $table) {
             if ($table->getStatus() === AbstractTable::STATUS_NEW) {
-                $result[] = [ChangeType::Create , $table->getName()];
+                $result[] = [ChangeType::Create, $table->getName()];
                 continue;
             }
 
             if ($table->getStatus() === AbstractTable::STATUS_DECLARED_DROPPED) {
-                $result[] = [ChangeType::Drop , $table->getName()];
+                $result[] = [ChangeType::Drop, $table->getName()];
                 continue;
             }
 
             if ($table->getComparator()->isRenamed()) {
-                $result[] = [ChangeType::Rename , $table->getInitialName()];
+                $result[] = [ChangeType::Rename, $table->getInitialName()];
                 continue;
             }
 
-            $result[] = [ChangeType::Change , $table->getName()];
+            $result[] = [ChangeType::Change, $table->getName()];
 
             $comparator = $table->getComparator();
 
             foreach ($comparator->addedColumns() as $column) {
-                $result[] = [ChangeType::Add , $column->getName()];
+                $result[] = [ChangeType::Add, $column->getName()];
             }
 
             foreach ($comparator->droppedColumns() as $column) {
-                $result[] = [ChangeType::Remove , $column->getName()];
+                $result[] = [ChangeType::Remove, $column->getName()];
             }
 
             foreach ($comparator->alteredColumns() as $column) {
-                $result[] = [ChangeType::Alter , $column[0]->getName()];
+                $result[] = [ChangeType::Alter, $column[0]->getName()];
             }
 
             foreach ($comparator->addedIndexes() as $index) {
-                $result[] = [ChangeType::AddIndex , $index->getName()];
+                $result[] = [ChangeType::AddIndex, $index->getName()];
             }
 
             foreach ($comparator->droppedIndexes() as $index) {
-                $result[] = [ChangeType::RemoveIndex , $index->getName()];
+                $result[] = [ChangeType::RemoveIndex, $index->getName()];
             }
 
             foreach ($comparator->alteredIndexes() as $index) {
-                $result[] = [ChangeType::AlterIndex , $index[0]->getName()];
+                $result[] = [ChangeType::AlterIndex, $index[0]->getName()];
             }
 
             foreach ($comparator->addedForeignKeys() as $fk) {
-                $result[] = [ChangeType::AddFk , $fk->getName()];
+                $result[] = [ChangeType::AddFk, $fk->getName()];
             }
 
             foreach ($comparator->droppedForeignKeys() as $fk) {
-                $result[] = [ChangeType::RemoveFk , $fk->getName()];
+                $result[] = [ChangeType::RemoveFk, $fk->getName()];
             }
 
             foreach ($comparator->alteredForeignKeys() as $fk) {
-                $result[] = [ChangeType::AlterFk , $fk[0]->getName()];
+                $result[] = [ChangeType::AlterFk, $fk[0]->getName()];
             }
         }
 
